b8cc739be386932ea79de7d2bef691139bc0d5b3
Gabriel <gabrieldecb@gmail.com>
2024-11-18 16:15:50 -0300

    "Corrigindo problema de simetria na matrizDistancias"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Long [][] matrizDistancias = {
-                {0L, 1L, 2L, 2L, 4L, 5L, 1L, 2L, 2L, 4L},
-                {1L, 0L, 1L, 2L, 4L, 5L, 1L, 2L, 2L, 4L},
-                {2L, 1L, 0L, 2L, 3L, 5L, 1L, 2L, 2L, 4L},
-                {2L, 2L, 2L, 0L, 2L, 5L, 1L, 2L, 2L, 4L},
-                {4L, 4L, 3L, 2L, 0L, 5L, 1L, 2L, 2L, 4L},
-                {4L, 4L, 3L, 2L, 4L, 0L, 1L, 2L, 2L, 4L},
-                {4L, 4L, 3L, 2L, 7L, 5L, 0L, 2L, 2L, 4L},
-                {4L, 4L, 3L, 2L, 8L, 5L, 1L, 0L, 2L, 4L},
-                {4L, 4L, 3L, 2L, 9L, 5L, 1L, 2L, 0L, 4L},
-                {4L, 4L, 3L, 2L, 6L, 5L, 1L, 2L, 2L, 0L}
+        Long[][] matrizDistancias = {
+                // A   B   C   D   E   F   G   H   I   J
+                {0L, 1L, 2L, 2L, 4L, 4L, 1L, 2L, 2L, 4L}, // A
+                {1L, 0L, 1L, 2L, 4L, 4L, 1L, 2L, 2L, 4L}, // B
+                {2L, 1L, 0L, 2L, 3L, 3L, 1L, 2L, 2L, 3L}, // C
+                {2L, 2L, 2L, 0L, 2L, 2L, 1L, 2L, 2L, 2L}, // D
+                {4L, 4L, 3L, 2L, 0L, 4L, 1L, 2L, 2L, 4L}, // E
+                {4L, 4L, 3L, 2L, 4L, 0L, 1L, 2L, 2L, 4L}, // F
+                {1L, 1L, 1L, 1L, 1L, 1L, 0L, 1L, 1L, 1L}, // G
+                {2L, 2L, 2L, 2L, 2L, 2L, 1L, 0L, 2L, 2L}, // H
+                {2L, 2L, 2L, 2L, 2L, 2L, 1L, 2L, 0L, 2L}, // I
+                {4L, 4L, 3L, 2L, 4L, 4L, 1L, 2L, 2L, 0L}  // J
         };
 
         List<String> enderecos = List.of("Endereço A", "Endereço B", "Endereço C", "Endereço D", "Endereço E", "Endereço F", "Endereço G", "Endereço H", "Endereço I", "Endereço J");
