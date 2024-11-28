1c276a2e3588563c504aff3ca21c2059428427e9
Gabriel <gabrieldecb@gmail.com>
2024-11-28 18:11:49 -0300

    "Modificado a incialização dos feromonios"

--- src/main/java/org/example/algorithm/Graph.java
+++ src/main/java/org/example/algorithm/Graph.java
@@ -16,11 +16,30 @@ public class Graph {
     }
 
     private void initializePheromones() {
+        double initialPheromone = 1.0 / calculateAverageDistance(); // baseado na média das distâncias
         for (int i = 0; i < nodes.size(); i++) {
             for (int j = 0; j < nodes.size(); j++) {
-                pheromones[i][j] = 1.0;
+                if (i != j) {
+                    pheromones[i][j] = initialPheromone;
+                } else {
+                    pheromones[i][j] = 0.0; // sem auto-loops
+                }
             }
         }
+    }
+
+    private double calculateAverageDistance() {
+        long sum = 0;
+        int count = 0;
+        for (int i = 0; i < matrixDistances.length; i++) {
+            for (int j = 0; j < matrixDistances[i].length; j++) {
+                if (i != j) {
+                    sum += matrixDistances[i][j];
+                    count++;
+                }
+            }
+        }
+        return count > 0 ? (double) sum / count : 1.0;
     }
 
     public List<Location> getNodes() {
